ec298d48dd005c7d87540c3b183c2ea9fef9105e
Ondrej Jirman <megous@megous.com>
2014-10-23 23:56:52 +0200

    Updated testsuite

--- tests/tools/06-test-dl.sh
+++ tests/tools/06-test-dl.sh
@@ -6,6 +6,6 @@ megadl --help
 LINK=`megals $ROPTS --export /Root/TestDir/test.dat | cut -d ' ' -f 1`
 
 rm -f test-dl.dat
-megadl --path test-dl.dat $LINK
+megadl --path test-dl.dat "$LINK"
 
 cmp test.dat test-dl.dat || echo "=== FAILED ==="
--- tests/tools/08-test-stream.sh
+++ tests/tools/08-test-stream.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 source ./config
 
-megastream --help
+LINK=`megals $ROPTS --export /Root/TestDir/test.dat | cut -d ' ' -f 1`
 
 rm -f test-stream.dat
-megastream $OPTS  /Root/TestDir/test.dat > test-stream.dat
+megaget $OPTS --path - /Root/TestDir/test.dat > test-stream.dat
+
+cmp test.dat test-stream.dat || echo "=== FAILED ==="
+
+rm -f test-stream.dat
+megadl --path - "$LINK" > test-stream.dat
 
 cmp test.dat test-stream.dat || echo "=== FAILED ==="
