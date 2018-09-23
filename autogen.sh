0346e76bf52a930c092d67f82c4f61cda0e79471
Ondrej Jirman <megous@megous.com>
2018-09-23 12:09:10 +0200

    Add git warning/build instructions to autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -12,3 +12,14 @@ autoreconf -v --install || exit 1
 cd $ORIGDIR || exit $?
 
 $srcdir/configure --enable-maintainer-mode --enable-warnings "$@"
+
+cat << EOF
+=========================================================================
+|           WARNING         You're building from GIT code               |
+=========================================================================
+| - Don't! git is for developers, release tarballs are for end users.   |
+|   Get them here:  https://megatools.megous.com/builds/                |
+| - Read the README for build instructions!                             |
+| - Random checkouts from git are not tested.                           |
+=========================================================================
+EOF
